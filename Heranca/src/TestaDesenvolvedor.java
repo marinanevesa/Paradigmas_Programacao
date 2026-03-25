e9094ebfabe1d2215a85ce952b073d2090d391d2
Marina Neves <marinanevesa@gmail.com>
2026-03-24 22:14:56 -0300

    Heranca

---
+++ Heranca/src/TestaDesenvolvedor.java
@@ -0,0 +1,7 @@
+public class TestaDesenvolvedor {
+    public static void main(String[] args){
+        Junior jrl = new Junior("João", "Java", 2000, "Maria");
+        Pleno pln = new Pleno("Maria", "Python", 4000, 5);
+        Senior snr = new Senior("Pedro", "C#", 6000, 10000);
+    }
+}
